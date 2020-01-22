a9b182178c65d882679b5f16f2cb991c309cf193
Shiny Zhu <zhushiny@gmail.com>
2020-01-22 13:39:36 +0800

    Revert "Merge branch 'master' into vuepress-src"
    This reverts commit 91900259e2bb7c136a1b2f77d8baade51991cdd2, reversing
    changes made to 4f7014a48a12b3438fbc4e60194eac2bfdfd0a8f.

---
+++ .vuepress/components/TagLinks.vue
@@ -0,0 +1,18 @@
+<!-- .vuepress/components/TagLinks.vue -->
+<template lang="html">
+  <div class="tags-head">
+    Tags:
+    <span>
+    <router-link
+      v-for="tag in $page.frontmatter.tags"
+      :key="tag"
+      :to="{ path: `/tags.html#${tag}`}">
+      {{tag}}
+    </router-link>
+    </span>
+  </div>
+</template>
+
+<style>
+.tags-head span a{margin:0 3px;padding:0 3px;background:#eee;line-height:1.5em}
+</style>
